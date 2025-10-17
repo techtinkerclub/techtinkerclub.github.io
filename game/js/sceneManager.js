37b2a5fb4461caf6a73f8b6df2c2ebc959c0bd4b
techtinkerclub <techtinkerclub@gmail.com>
2025-10-17 13:28:03 +0100

    Delete game/js directory

--- game/js/sceneManager.js
+++
@@ -1,8 +0,0 @@
-class SceneManager {
-  constructor() { this.current = null; }
-  set(scene) { this.current = scene; this.current.init?.(); }
-  update() { this.current?.update?.(); }
-  draw()   { this.current?.draw?.(); }
-  keyPressed(k) { this.current?.keyPressed?.(k); }
-  mousePressed() { this.current?.mousePressed?.(); }
-}
