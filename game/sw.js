92f0ecdda08b627a30bb1437dbd7abab0471abad
techtinkerclub <techtinkerclub@gmail.com>
2026-09-22 20:42:19 +0100

    Bump game cache for viewport fix

--- game/sw.js
+++ game/sw.js
@@ -1,8 +1,8 @@
-/* Tech Tinker: System Rescue service worker v1.30.0 */
+/* Tech Tinker: System Rescue service worker v1.31.0 */
 'use strict';
 
 const CACHE_PREFIX = 'tt-system-rescue-';
-const CACHE_NAME = CACHE_PREFIX + 'v1.30.0';
+const CACHE_NAME = CACHE_PREFIX + 'v1.31.0';
 const APP_ROOT = '/game/';
 const CORE = [
   '/game/',
