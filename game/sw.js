e0b2c04a4fe8c0c958dc15ec37106ccf07389264
techtinkerclub <techtinkerclub@gmail.com>
2026-09-22 09:23:25 +0100

    Bump cache for desktop workspace update

--- game/sw.js
+++ game/sw.js
@@ -1,8 +1,8 @@
-/* Tech Tinker: System Rescue service worker v1.13.0 */
+/* Tech Tinker: System Rescue service worker v1.14.0 */
 'use strict';
 
 const CACHE_PREFIX = 'tt-system-rescue-';
-const CACHE_NAME = CACHE_PREFIX + 'v1.13.0';
+const CACHE_NAME = CACHE_PREFIX + 'v1.14.0';
 const APP_ROOT = '/game/';
 const CORE = [
   '/game/',
