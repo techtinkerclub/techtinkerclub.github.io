99d57cb3b8ea0b89e861bf184ff503c0aaf785a2
techtinkerclub <techtinkerclub@gmail.com>
2026-09-22 16:17:35 +0100

    Bump cache for expedition prototype

--- game/sw.js
+++ game/sw.js
@@ -1,8 +1,8 @@
-/* Tech Tinker: System Rescue service worker v1.26.0 */
+/* Tech Tinker: System Rescue service worker v1.27.0 */
 'use strict';
 
 const CACHE_PREFIX = 'tt-system-rescue-';
-const CACHE_NAME = CACHE_PREFIX + 'v1.26.0';
+const CACHE_NAME = CACHE_PREFIX + 'v1.27.0';
 const APP_ROOT = '/game/';
 const CORE = [
   '/game/',
