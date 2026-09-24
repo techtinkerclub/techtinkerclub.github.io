a09e02b96dea8ac03249e4b891588308c98a5f72
techtinkerclub <techtinkerclub@gmail.com>
2026-09-24 12:53:13 +0100

    Bump game cache to v1.44.0

--- game/sw.js
+++ game/sw.js
@@ -1,8 +1,8 @@
-/* Tech Tinker: System Rescue service worker v1.43.0 */
+/* Tech Tinker: System Rescue service worker v1.44.0 */
 'use strict';
 
 const CACHE_PREFIX = 'tt-system-rescue-';
-const CACHE_NAME = CACHE_PREFIX + 'v1.43.0';
+const CACHE_NAME = CACHE_PREFIX + 'v1.44.0';
 const APP_ROOT = '/game/';
 const CORE = [
   '/game/',
